bac59324934b453562b9bc9f11530171e0b6633c
herly <herlysarmiento@gmail.com>
2020-02-24 16:27:25 -0700

    finished navigation and added active class

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -9,20 +9,19 @@ export default class NavigationComponent extends Component {
   render() {
     return (
       <div>
-        <NavLink exact to="/">
+        <NavLink exact to="/"activeClassName="nav-link-active">
           Home
         </NavLink>
-        <NavLink exact to="/about-me">
+        <NavLink exact to="/about-me" activeClassName="nav-link-active">
           About
         </NavLink>
-        
-        
+        <NavLink exact to="/contact" activeClassName="nav-link-active">
+          Contact Us
+        </NavLink>
+        <NavLink exact to="/blog" activeClassName="nav-link-active">
+          Blog
+        </NavLink>
 
-
-        <button>Home</button>
-        <button>About</button>
-        <button>Contact</button>
-        <button>Blog</button>
         {false ? <button>Add Blog</button> : null}
       </div>
     );
