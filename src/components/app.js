a00322ced5cc1198086f0281b5089b039420fcff
herly <herlysarmiento@gmail.com>
2020-02-25 16:04:40 -0700

    rendering live portfolio data

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import moment from "moment";
-
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationContainer from "./navigation/navigation-container";
@@ -12,11 +11,7 @@ import PortfolioDetail from "./portfolio/portfolio-detail";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
-  
-
-  
   render() {
-    
     return (
       <div className="app">
         <Router>
@@ -30,7 +25,11 @@ export default class App extends Component {
               <Route path="/about-me" component={About} />
               <Route path="/contact" component={Contact} />
               <Route path="/blog" component={Blog} />
-              <Route path="/portfolio/:slug" component={PortfolioDetail}/>
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
               <Route component={NoMatch} />
             </Switch>
           </div>
--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,71 +1,80 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
 
-import PortfolioItem from "./portfolio-item"
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
-    constructor() {
-        super();
+  constructor() {
+    super();
 
-        this.state = {
-            pageTitle: "Welcome to my portforlio",
-            isLoading: false,
-            data: [
-                {tittle: "Quip", category: "eCommerce", slug:'quip'},
-                {title: "Eventbrite", category: "Scheduling", slug: 'eventbrite'}, 
-                {title: "Ministry Safe", category: "Enterprise", slug:'ministry-safe'},
-                {title: "SwingAway", category: "eCommerce", slug:'swingaway'}
-            ]
-        };
-        this.handleFilter = this.handleFilter.bind(this);
-        this.getPortfolioItems = this.getPortfolioItems.bind(this);
-    }
-    handleFilter(filter) {
+    this.state = {
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    this.setState({
+      data: this.state.data.filter(item => {
+        return item.category === filter;
+      })
+    });
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://herlysarmiento.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
         this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
+          data: response.data.portfolio_items
         });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  portfolioItems() {
+    return this.state.data.map(item => {
+      return (
+        <PortfolioItem
+          key={item.id}
+          title={item.name}
+          url={item.url}
+          slug={item.id}
+        />
+      );
+    });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
     }
-    
 
-    getPortfolioItems() {
-        axios
-          .get("https://herlysarmiento.devcamp.space/portfolio/portfolio_items")
-          .then(response => {
-            console.log("response data", response);
-          })
-          .catch(error => {
-            console.log(error);
-          });
-    }
+    return (
+      <div>
+        <h2>{this.state.pageTitle}</h2>
 
-    portfolioItem() {
-        return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} slug={item.slug} />;
-        });
-    }
+        <button onClick={() => this.handleFilter("eCommerce")}>
+          eCommerce
+        </button>
+        <button onClick={() => this.handleFilter("Scheduling")}>
+          Scheduling
+        </button>
+        <button onClick={() => this.handleFilter("Enterprise")}>
+          Enterprise
+        </button>
 
-    
-    render() {
-        if (this.state.isLoading) {
-            return <div>Loading...</div>
-        }
-
-        this.getPortfolioItems();
-
-        return (
-            <div >
-                <h2>{this.state.pageTitle}</h2>
-
-                <button onClick={() => this.handleFilter('eCommerce')}>eCommerce</button>
-                <button onClick={() => this.handleFilter('Scheduling')}>Scheduling</button>
-                <button onClick={() => this.handleFilter('Enterprise')}>Enterprise</button>
-
-                {this.portfolioItem()}
-
-                
-            </div>
-        )
-    }
+        {this.portfolioItems()}
+      </div>
+    );
+  }
 }
