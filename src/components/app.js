bf67d70e52d45b7b36d7add162ef6c30ddc4d559
herly <herlysarmiento@gmail.com>
2020-02-25 12:01:24 -0700

    Added Axios and added a call to the devcamp space to get my portfolio items

--- src/components/app.js
+++ src/components/app.js
@@ -1,43 +1,53 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import moment from "moment";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-
+import axios from "axios";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
-import PortfolioDetail from "./portfolio/portfolio-details";
+import PortfolioDetail from "./portfolio/portfolio-detail";
 import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
+  constructor() {
+    super();
+
+    this.getPortfolioItems = this.getPortfolioItems.bind(this);
+  }
+
+  getPortfolioItems() {
+    axios
+      .get("https://herlysarmiento.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        console.log("response data", response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
   render() {
+    this.getPortfolioItems();
     return (
-      <div className='app'>
-        
+      <div className="app">
         <Router>
           <div>
-          <h1>Herly Sarmiento Portfolio</h1>
-          <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+            <h1>Herly Sarmiento Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
             <NavigationContainer />
+
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path ="/about-me" component={About} />
-              <Route path ="/contact" component={Contact} />
-              <Route path ="/blog" component={Blog} />
-              <Route path ="/portfolio/:slug" component={PortfolioDetail} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route path="/portfolio/:slug" component={PortfolioDetail}/>
               <Route component={NoMatch} />
-
             </Switch>
           </div>
         </Router>
-        
-             
       </div>
     );
   }
