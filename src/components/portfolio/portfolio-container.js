8351c13563bcaacb0f9f604712acc43b5f8a385d
herly <herlysarmiento@gmail.com>
2020-02-27 11:51:26 -0700

    added auth scss

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -54,14 +54,14 @@ export default class PortfolioContainer extends Component {
 
     return (
       <div className="portfolio-items-wrapper">
-        <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
-          eCommerce
+        <button className="btn" onClick={() => this.handleFilter("PYTHON")}>
+          Python
         </button>
-        <button className="btn" onClick={() => this.handleFilter("Scheduling")}>
-          Scheduling
+        <button className="btn" onClick={() => this.handleFilter("Technology")}>
+          Technology
         </button>
-        <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
-          Enterprise
+        <button className="btn" onClick={() => this.handleFilter("HTML-CSS")}>
+          HTML-CSS
         </button>
 
         {this.portfolioItems()}
