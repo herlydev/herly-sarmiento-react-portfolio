c98d0178b63c9a7918bf98571f3eabbdbfc0c564
herly <herlysarmiento@gmail.com>
2020-02-27 13:43:02 -0700

    added login form working from handle change but not submit

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,13 +1,55 @@
 import React, { Component } from "react";
 
 export default class Auth extends Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    console.log("hanle change");
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log("handle submit", event);
+  }
+
   render() {
     return (
       <div>
         <h1>Login to Acces your Dashboard</h1>
-        <form>
-          <input type="text" />
-          <input type="password" />
+
+        <h2>{this.state.email}</h2>
+        <h2>{this.state.password}</h2>
+
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Youe email"
+            value={this.state.email}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="Your password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+
+          <div>
+            <button type="submit">Login</button>
+          </div>
         </form>
       </div>
     );
