b07dc97387a5b46df0066866f9e707f4fb7757d3
herly <herlysarmiento@gmail.com>
2020-02-27 12:01:49 -0700

    added login js file including dashboard form

---
+++ src/components/auth/login.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export default class Auth extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Login to Acces your Dashboard</h1>
+        <form>
+          <input type="text" />
+          <input type="password" />
+        </form>
+      </div>
+    );
+  }
+}
--- src/components/pages/auth.js
+++ src/components/pages/auth.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import loginImg from "../../../static/assets/images/auth/login.jpg";
 
+import Login from "../auth/login";
+
 export default class Auth extends Component {
   render() {
     return (
@@ -13,7 +15,7 @@ export default class Auth extends Component {
         />
 
         <div className="right-column">
-          <h1>Login component goes here...</h1>
+          <Login />
         </div>
       </div>
     );
