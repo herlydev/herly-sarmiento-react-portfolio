d5bf0333e7a75885cf25810999bca6ffcd5ef3e9
herly <herlysarmiento@gmail.com>
2020-03-05 16:16:23 -0700

    calling the blog API and storing the data in state

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
-export default function() {
-  return (
-    <div>
-      <h2>Blog</h2>
+class Blog extends Component {
+  constructor() {
+    super();
+    this.state = {
+      blogitems: []
+    };
 
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://herlysarmiento.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true
+      })
+      .then(response => {
+        // console.log("response", response);
+        this.setState({ blogitems: response.data.portfolio_blogs });
+      })
+      .catch(error => {
+        console.log("getBlogItems error", error);
+      });
+  }
+
+  componentDidMount() {
+    this.getBlogItems();
+  }
+  render() {
+    return (
       <div>
-        <Link to="/about-me">Read more about myself</Link>
+        <h2>Blog</h2>
+
+        <div>
+          <Link to="/about-me">Read more about myself</Link>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Blog;
