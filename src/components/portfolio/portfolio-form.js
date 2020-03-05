b325fc68b8e2d9d17e39fb1270d757ba099fab83
herly <herlysarmiento@gmail.com>
2020-03-05 13:51:48 -0700

    adding delete img link to the thumbnail

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -30,10 +30,14 @@ export default class PortfolioForm extends Component {
     this.handleThumbDrop = this.handleThumbDrop.bind(this);
     this.handleBannerDrop = this.handleBannerDrop.bind(this);
     this.handleLogoDrop = this.handleLogoDrop.bind(this);
+    this.deleteImage = this.deleteImage.bind(this);
 
     this.thumbRef = React.createRef();
     this.bannerRef = React.createRef();
     this.logoRef = React.createRef();
+  }
+  deleteImage(imageType) {
+    console.log("delete image", imageType);
   }
 
   componentDidUpdate() {
@@ -228,6 +232,12 @@ export default class PortfolioForm extends Component {
           {this.state.thumb_image && this.state.editMode ? (
             <div className="portfolio-manager-image-wrapper">
               <img src={this.state.thumb_image} />
+
+              <div className="image-removal-link">
+                <a onClick={() => this.deleteImage("thumb_image")}>
+                  Remove file
+                </a>
+              </div>
             </div>
           ) : (
             <DropzoneComponent
