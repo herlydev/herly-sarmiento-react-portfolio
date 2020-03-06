efc51e57a36e0cb93179fd0c56d5b72fd08d6483
herly <herlysarmiento@gmail.com>
2020-03-06 15:27:57 -0700

    full infinite scroll funcionality

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -22,10 +22,17 @@ class Blog extends Component {
   activateInfiniteScroll() {
     window.onscroll = () => {
       if (
+        this.state.isLoading ||
+        this.state.blogItems.length === this.state.totalCount
+      ) {
+        return;
+      }
+
+      if (
         window.innerHeight + document.documentElement.scrollTop ===
         document.documentElement.offsetHeight
       ) {
-        console.log("get more posts");
+        this.getBlogItems();
       }
     };
   }
@@ -34,13 +41,18 @@ class Blog extends Component {
     this.setState({
       currentPage: this.state.currentPage + 1
     });
+
     axios
-      .get("https://herlysarmiento.devcamp.space/portfolio/portfolio_blogs", {
-        withCredentials: true
-      })
+      .get(
+        `https://herlysarmiento.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
+        {
+          withCredentials: true
+        }
+      )
       .then(response => {
+        console.log("gettting", response.data);
         this.setState({
-          blogItems: response.data.portfolio_blogs,
+          blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
           totalCount: response.data.meta.total_records,
           isLoading: false
         });
@@ -62,6 +74,7 @@ class Blog extends Component {
     return (
       <div className="blog-container">
         <div className="content-container">{blogRecords}</div>
+
         {this.state.isLoading ? (
           <div className="content-loader">
             <FontAwesomeIcon icon="spinner" spin />
